a30059d0ad8175aeeab975bdce0ff3b97e34ff3e
Lukas Forer <lukas.forer@i-med.ac.at>
2022-02-28 12:29:22 +0100

    Fix double bracket in legend

--- src/main/resources/web/queries/show.succeded.view.js
+++ src/main/resources/web/queries/show.succeded.view.js
@@ -34,7 +34,7 @@ function createTrace(data, index, filter) {
     x: x,
     y: y,
     type: 'scatter',
-    name: data.subDataset.name + ')',
+    name: data.subDataset.name,
     line: {
       width: 2,
       color: colors(data.subDataset.reference),
