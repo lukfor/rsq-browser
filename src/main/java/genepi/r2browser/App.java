670d06c0409cc0ab004114c9f31a1177e8a4f584
Lukas Forer <lukas.forer@i-med.ac.at>
2023-09-22 16:17:24 +0200

    Prepare release 0.5.4

--- src/main/java/genepi/r2browser/App.java
+++ src/main/java/genepi/r2browser/App.java
@@ -16,7 +16,7 @@ public class App implements Runnable {
 
 	public static final String NAME = "rsq-browser";
 
-	public static final String VERSION = "0.5.3";
+	public static final String VERSION = "0.5.4";
 
 	public static final String COPYRIGHT = "";
 
