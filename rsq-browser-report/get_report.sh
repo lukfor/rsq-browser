c5ac4b6553429048635506aabd07fcc0cbfea6bc
AUT-FS97 <flo.schnitzer@gmail.com>
2024-12-04 16:51:46 +0100

    add environment.yml file with the required conda env plus rendering with quarto with parameters from .yml file

--- rsq-browser-report/get_report.sh
+++ rsq-browser-report/get_report.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-quarto preview report.Rmd --to html --no-watch-inputs --no-browse
+quarto render report.qmd --to html --execute-params test_input.yml
+#quarto render report.qmd --to html
+#quarto preview report.Rmd --to html --no-watch-inputs --no-browse
 #Rscript -e "rmarkdown::render('report.Rmd')"
 #Rscript -e "rmarkdown::render('example.Rmd',params=list(args = myarg))"
