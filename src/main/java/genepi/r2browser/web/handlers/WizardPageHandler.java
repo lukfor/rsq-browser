ccf3335784dc37155cfb8c405a5d3242d1d69d4a
Lukas Forer <lukas.forer@i-med.ac.at>
2024-12-14 12:16:00 +0100

    Move form to report

--- src/main/java/genepi/r2browser/web/handlers/WizardPageHandler.java
+++ src/main/java/genepi/r2browser/web/handlers/WizardPageHandler.java
@@ -3,9 +3,14 @@ package genepi.r2browser.web.handlers;
 import genepi.r2browser.App;
 import genepi.r2browser.config.Configuration;
 import genepi.r2browser.web.util.AbstractHandler;
+import genepi.r2browser.web.util.BasisTemplateFileRenderer;
 import genepi.r2browser.web.util.Page;
 import io.javalin.http.Context;
 import io.javalin.http.HandlerType;
+import io.javalin.http.staticfiles.Location;
+import io.marioslab.basis.template.Template;
+import io.marioslab.basis.template.TemplateContext;
+import io.marioslab.basis.template.TemplateLoader;
 
 public class WizardPageHandler extends AbstractHandler {
 
@@ -19,10 +24,24 @@ public class WizardPageHandler extends AbstractHandler {
 
 	public void handle(Context context) throws Exception {
 
+		String report = "wizard";
+
+		TemplateLoader.FileTemplateLoader loader = new TemplateLoader.FileTemplateLoader();
+		TemplateContext templateContext = new TemplateContext();
+		templateContext.set("post_url", configuration.getBaseUrl() +  "/reports");
+		templateContext.set("configuration", configuration);
+
+		Template template =  loader.load("reports/" + report + "/form.html");
+		String form = template.render(templateContext);
+
+		Template templateJs =  loader.load("reports/" + report + "/form.js");
+		String formJs = templateJs.render(templateContext);
+
 		Page page = new Page(context, TEMPLATE);
 		page.put("configuration", configuration);
+		page.put("form", form);
+		page.put("script", formJs);
 		page.render();
-
 	}
 
 	@Override
